9d6c84a08805f192ecdeba219eac1a3d0bde58cd
Tyler Cook <cooktylerjames@gmail.com>
2017-10-11 10:05:01 -0600

    capitalize first letter in string

--- src/add.js
+++ src/add.js
@@ -7,3 +7,27 @@
 function liftoff(instructions){
     return instructions.sort((a, b) => b - a).join(' ') + ' liftoff!';
   }
+
+
+
+  //Jaden Smith, the son of Will Smith, is the star of films such as The Karate Kid (2010) and After Earth (2013). Jaden is also known for some of his philosophy that he delivers via Twitter. When writing on Twitter, he is known for almost always capitalizing every word.
+  
+  //Your task is to convert strings to how they would be written by Jaden Smith. The strings are actual quotes from Jaden Smith, but they are not capitalized in the same way he originally typed them.
+
+
+
+  String.prototype.toJadenCase = function () {
+    //Try to split out into an array
+    var arr = this.split(' ')
+    
+    //Loop through it and cap the first letter
+    var result = arr.map(item => {
+        return item.charAt(0).toUpperCase() + item.slice(1)
+    
+    
+    })
+    
+    return result.join(' ')
+    
+    
+  };
